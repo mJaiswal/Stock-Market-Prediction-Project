c5d57fcfd74099fccbf6f11f71be2dfa2d970238
Thirunavukkarasu Arunachalam <thiru650@gmail.com>
2015-03-03 22:48:40 +0000

    CreateScripts.sql edited online with Bitbucket

--- CreateScripts.sql
+++ CreateScripts.sql
@@ -1,25 +1,30 @@
-CREATE TABLE stocks(
-   stock_id serial PRIMARY KEY NOT NULL,
-   symbol varchar(10) unique not null,
+drop table if exists real_time_data;
+drop table if exists historical_data;
+drop table if exists stocks;
+
+CREATE TABLE stocks(   
+   symbol varchar(10) PRIMARY KEY NOT NULL,
    name varchar(30) NOT NULL
 );
 
 CREATE TABLE real_time_data(
-   stock_id integer references stocks(stock_id),
+   symbol varchar(10) references stocks(symbol),
    date date,
    time time,
    price NUMERIC(10, 2),   
    volume integer,
-   PRIMARY KEY (stock_id, date, time)
+   adj_close_price NUMERIC(10,2),
+   PRIMARY KEY (symbol, date, time)
 );
 
 CREATE TABLE historical_data(
-   stock_id integer references stocks(stock_id),
+   symbol varchar(10) references stocks(symbol),
    date date,
    open_price NUMERIC(10, 2),
    close_price NUMERIC(10, 2),
    high_price NUMERIC(10, 2),
    low_price NUMERIC(10, 2),
    volume integer,
-   PRIMARY KEY (stock_id, date)
+   adj_close_price NUMERIC(10,2),
+   PRIMARY KEY (symbol, date)
 );
